3b4cba47101c8aa900580f3a02a292327d2b7110
Emmanuel <emmanueljoeemie@gmail.com>
2024-06-12 19:42:14 +0100

    implemented webhook...for user event on clerk triggers..added user server action

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,19 @@
+import { model } from "mongoose";
+import { models } from "mongoose";
+import { Document, Schema } from "mongoose";
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Category = models.Category || model("Category", CategorySchema);
+
+export default Category;
