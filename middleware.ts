94c2caf90b57a1b636a6507385113ef52f9f1b79
Emmanuel <emmanueljoeemie@gmail.com>
2024-06-13 16:34:27 +0100

    ...

--- middleware.ts
+++ middleware.ts
@@ -35,7 +35,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/events/:id",
-  "/api/webhook/clerk",
+  "/api/webhooks/clerk",
   "/api/webhook/stripe",
   "/api/uploadthing",'/sign-in', '/sign-up'
 ]);
