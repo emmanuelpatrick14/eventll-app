b69f105b8a46e67cc0d7b6a3e5f9c2427198bad9
Emmanuel <emmanueljoeemie@gmail.com>
2024-06-26 11:06:03 +0100

    ...

--- lib/database/models/userModel.ts
+++ lib/database/models/userModel.ts
@@ -2,31 +2,30 @@ import { model, models } from "mongoose";
 import { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkId:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    username:{
-       type:String,
-        required:true,
-        unique:true 
-    },
-    firstName:{
-        type:String,
-        required:true
-    },
-    photo:{
-        type:String,
-        required:false
-    }
-
-})
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+});
 
 const User = models.User || model("User", UserSchema);
 
